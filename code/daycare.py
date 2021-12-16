8654aa00ac87c941f19758d383c626ac907a6b0f
Kaiser <54066349+Ihre-Kaiserliche-Hoheit@users.noreply.github.com>
2021-12-16 13:43:14 +0100

    Moved Vars

--- code/daycare.py
+++ code/daycare.py
@@ -9,18 +9,4 @@ class daycare:
 	def __init__(self, name, randomNameFilePath:str, helpFilePath:str):
 		self.name = name
 
-		self.month = 1 # Time is given in months
-
-		self.wealth = 50
-
-		self.isDebugOn = True
-
-		self.species = dict()
-
-		self.breedingDifficulty = 25 # The n out of 100 chance to successfully reproduce
-
-		self.randomNames = load(open(randomNameFilePath, "r"))
-		self.help = open(helpFilePath, "r").read()
-		self.genes = load(open("../input/genetics.json"))["geneBlocks"]
-		self.geneValues = ["fluff", "speed", "size"]
-		self.alleles = load(open("../input/genetics.json"))["possibleGenes"]
+		self.upgrades = dict()
